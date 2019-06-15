0fb8cdef3c605c592b158dab2f1efeb24277c65b
diberry <diberry@microsoft.com>
2019-06-15 11:17:31 -0700

    abs v4 update

---
+++ documentation-samples/tutorial-web-app-bot/readme.md
@@ -0,0 +1,6 @@
+# Web app bot using Language Understanding
+
+|Language|
+|--|
+|[C#](./v4/luis-csharp-bot-johnsmith-src/readme.md)|
+|[Node.js](./v4/luis-nodejs-bot-johnsmith-src/readme.md)
---
+++ documentation-samples/tutorial-web-app-bot/v4/luis-csharp-bot-johnsmith-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=luis-csharp-bot-johnsmith-Web-Deploy.pubxml -p:Password=bSJFJvThkqEy0L5QFropuvraRFxf5aE4HsmlMxbvEkr3JMxuQz4frnu5cDwM
+
